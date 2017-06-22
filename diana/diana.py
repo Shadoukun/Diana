5ed922a7128059301737343b7875012804b6841f
Unknown <shadoukun@gmail.com>
2017-06-22 16:21:12 -0400

    I should test stuff before pushing it.

--- diana/diana.py
+++ diana/diana.py
@@ -66,7 +66,8 @@ class Diana(commands.Bot):
 
         # check for trigger in message
         for word in message.split(" "):
-            if word.lower() in self.responses:
+            word = word.lower()
+            if word in self.responses:
                 resp = self.responses[word].split('\n')
 
                 # check if there are multiple possible responses
